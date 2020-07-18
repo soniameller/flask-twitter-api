64b9205c7a4b2c51de112bc97bfb1979faabb0e2
Sonia Meller <mellersonia@gmail.com>
2020-07-18 16:25:11 +0100

    Added procfile

--- api.py
+++ api.py
@@ -76,4 +76,6 @@ def api_tweets():
     # Python dictionaries to the JSON format.
     return jsonify({'tweets':result})
 
-app.run()
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
